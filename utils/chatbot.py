61cdcb9fbf43c5432cf025ebe3c95e8e5a274c23
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2024-04-06 20:16:00 -0400

    Delete utils directory

--- utils/chatbot.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline, Conversation
-
-
-def get_chatbot_response(user_input):
-    chatbot = pipeline("conversational", model="microsoft/DialoGPT-large")
-    conversation = Conversation(user_input)
-    chatbot(conversation)
-    return conversation.generated_responses[-1]
